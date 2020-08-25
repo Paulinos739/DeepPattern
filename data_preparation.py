0bc0edb6e974dd2607c14e2ab7d2df354b7e32ce
Paul Steggemann <65179419+Paulinos739@users.noreply.github.com>
2020-08-25 21:45:43 +0200

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -2,12 +2,13 @@
 from skimage.transform import resize, rescale
 import matplotlib.pyplot as plt
 
+#Resize the floor plan images 
 new_image = plt.imread(img_path)
 resized_image = resize(new_image, (256,256,3))
 img = plt.imshow(resized_image)
 
-#Resize function - proportionally
+#Resize function - but proportionally
 new_image = plt.imread(img_path)
-resized_image = resize(new_image, (new_image.shape[0] // 6, new_image.shape[1] // 6),
+resized_image = resize(new_image, (new_image.shape[0] // 6, new_image.shape[1] // 6),   ##Which resolution does this resize function otput?
                        anti_aliasing=True)
-img= plt.imshow(resized_image)
+img= plt.imshow(resized_image)    
