c9ffb84f12a5b7650e06c083366e6fb69e8fc000
paulinos <paul.steggemann@stud.tu-darmstadt.de>
2021-08-23 16:57:58 +0200

    changed functions

--- predictions/multiple_floor_plans_predict.py
+++ predictions/multiple_floor_plans_predict.py
@@ -16,45 +16,32 @@ from keras.preprocessing import image
 
 
 # Model to distinguish with multi-label classification
-
 def classifier_multi_label():
-    classifier = load_model(
-        r'C:\Users\PAUL\PycharmProjects\FM_SoSo20_Master\trained_classifiers_weights\multi_label\Multi_label_100_CNN.h5',
+    classifier = load_model('path/to/model',
         compile=True)
-
     return classifier
 
 
-def main():
-
-    def floor_plan_prediction_multiple():
+ def floor_plan_prediction_multiple():
         # image folder
-        folder_path = 'path to folder containing the test data'
+        folder_path = 'path/to/folder/containing/test/data'
 
-        # dimensions of images
-        img_width, img_height = 64, 64
-
-        # load the trained model
-        model = classifier_multi_label()
-
-        # put all images into a list
+        # put all images into a list and...
         images = []
         for img in os.listdir(folder_path):
             img = os.path.join(folder_path, img)
-            img = image.load_img(img, target_size=(img_width, img_height))
+            img = image.load_img(img, target_size=(64, 64))
             img = image.img_to_array(img)
             img = np.expand_dims(img, axis=0)
             images.append(img)
 
-        # stack up images list to pass for prediction
+        # ...stack up images list to pass for prediction
         images = np.vstack(images)
-        result = model.predict(images, batch_size=None, verbose=1)  # or predict_classes when using only one model
+
+        # Run Prediction
+        result = classifier_multi_label().predict(images)
         print(result)
 
-    # Call the function
-    floor_plan_prediction_multiple()
 
-
-# Run Visualizer
 if __name__ == '__main__':
-    main()
+    floor_plan_prediction_multiple()
